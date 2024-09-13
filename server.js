a7f4b7f82c812cfb0201a2304cd4e7d9895cc5f9
git <dromer114@west-mec.org>
2024-09-13 14:28:46 -0700

    end of day 9/13

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
-const path = require('path');
+const app = express();
 const fs = require('fs').promises;
+
+app.get('/forms', (req, res) => {
+    res.sendFile('public/index.html', { root: clientPath });
+});
