6959cc4314e6144e38618f6d741051540d901c20
git <dromer114@west-mec.org>
2024-09-27 13:36:45 -0700

    start 9/27

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const fs = require('fs').promises;
-
-app.get('/forms', (req, res) => {
-    res.sendFile('public/index.html', { root: clientPath });
-});
